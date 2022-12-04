8e8f73ec2f10695e7de402cd5bfad479e76c2ca4
Seth Duffin <sethduffin@gmail.com>
2022-12-03 18:29:55 -0700

    Add page-link element

---
+++ elements/PageLink.js
@@ -0,0 +1,35 @@
+export class PageLink extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const { name, path, description = '', disabled } = getAttributesFrom(this);
+  
+    this.innerHTML = `
+      <li class="page-link">
+        <a href="${path}" class="${disabled && 'disabled'}">
+          <div class="page-link__text-container">
+            <h3>${name}</h3>
+            <p>${description}</p>
+          </div>
+          <span class="material-icons-round page-link__arrow">
+          arrow_forward
+          </span>
+        </a>
+      </li>
+    `;
+  }
+}
+
+function getAttributesFrom(elem) {
+  const attributeEntries = elem.getAttributeNames().map((name) => {
+    return [name, elem.getAttribute(name)];
+  });
+
+  const attributes = Object.fromEntries(attributeEntries);
+  
+  return attributes;
+}
+
+customElements.define('page-link', PageLink);
---
+++ elements/main.js
@@ -0,0 +1,7 @@
+import './PageLink.js';
+
+const linkElem = document.createElement("link");
+linkElem.setAttribute("rel", "stylesheet");
+linkElem.setAttribute("href", "/elements/main.css");
+
+document.head.appendChild(linkElem);
